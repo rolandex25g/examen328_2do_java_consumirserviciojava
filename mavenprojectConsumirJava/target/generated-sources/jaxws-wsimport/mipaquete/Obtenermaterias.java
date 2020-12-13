9e3b7a01b4d62edccbc57a3c83d06e473b8ede62
rolandex25 <rolandex25@gmail.com>
2020-12-13 12:17:57 -0400

    first commit

---
+++ mavenprojectConsumirJava/target/generated-sources/jaxws-wsimport/mipaquete/Obtenermaterias.java
@@ -0,0 +1,60 @@
+
+package mipaquete;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Clase Java para obtenermaterias complex type.
+ * 
+ * <p>El siguiente fragmento de esquema especifica el contenido que se espera que haya en esta clase.
+ * 
+ * <pre>
+ * &lt;complexType name="obtenermaterias">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="ci" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "obtenermaterias", propOrder = {
+    "ci"
+})
+public class Obtenermaterias {
+
+    protected String ci;
+
+    /**
+     * Obtiene el valor de la propiedad ci.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getCi() {
+        return ci;
+    }
+
+    /**
+     * Define el valor de la propiedad ci.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setCi(String value) {
+        this.ci = value;
+    }
+
+}
---
+++ mavenprojectConsumirJava/target/generated-sources/jaxws-wsimport/mipaquete/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://mipaquete/")
+package mipaquete;
